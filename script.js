e8e8199731fdc3949999919b4928020d402d764e
Rama Rahmat <ramarahmat41@gmail.com>
2025-09-07 10:39:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.addEventListener("keydown", (e) => {
     alert("Spotlight search is coming soon ✨");
   }
 });
+document.addEventListener("keydown", (e) => {
+  if ((e.metaKey || e.shiftKey) && e.key.toLowerCase() === "k") {
+    e.preventDefault();
+    alert(`
+    Changelog V1.0.1 ✨
+    - New UI 
+    - New Visual 
+    - 10 Games Added 
+    - Optimize Bugs`);
+  }
+});
